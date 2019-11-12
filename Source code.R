c77a756e9cd6a3638d1ffba7e974aae83198dd6d
Raka <56627679+rakaadi@users.noreply.github.com>
2019-11-12 16:22:49 +0700

    Add file via upload
    Upload the project to github

---
+++ Source code.R
@@ -0,0 +1,60 @@
+# Library yang digunakan
+library(readr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(tidyverse)
+
+# Membaca file csv
+budget <- read_csv2("January-august_2019.csv", col_types = cols(
+  Date = col_date(format = "%m/%d/%Y"),
+  Account = col_factor(),
+  Amount = col_number()
+))
+head(budget)
+
+# Merapihkan kolom
+  # Menghilangkan kolom 'Comment'
+spending_data <- select(budget, -Comment)
+head(spending_data)
+
+  # Mengubah nama kolom Main category
+  # dan menghilangkan nilai NA di kolom Subcategory
+spending_data <- rename(spending_data, Main_category = 'Main category')
+spending_data <- replace_na(spending_data, list(Subcategory = "None"))
+
+  # Menghitung nilai absolut dari kolom Amount,
+  # untuk menghilangkan nilai negatif
+spending_data$Amount <- abs(spending_data$Amount)
+
+# kode di bawah mungkin tidak akan dipakai
+# Menggabungkan kolom Main_category dan Subcategory
+spending_data2 <- unite(
+  spending_data, Category, 'Main_category', 'Subcategory', 
+  sep = ', ', remove = TRUE)
+
+# Mengambil data investasi dan menghilangkan kolom Subcategory
+investment <- filter(spending_data, Main_category == 'Investment')
+investment <- select(investment, -Subcategory)
+
+# Mengambil data pemasukan 
+income <- filter(spending_data, Main_category == "Income")
+
+# Mengubah isi dari kolom subcategory, mungkin tidak dipakai
+
+# Mengelompokan data berdasarkan bulan
+# Kode ini belum berhasil sesuai harapan
+month_investment <- investment %>%
+  mutate_at(investment, Date, format(investment$Date, "%m%Y")) %>%
+  group_by(month_investment, Date) %>%
+  summarise(Amount=sum(Amount))
+
+# Menjumlahkan pengeluaran untuk persatu hari
+gdate_spending <- group_by(spending_data, Date) %>%
+  summarise_if(is.numeric, sum)
+
+# Plotting
+  # investment
+ggplot(data = investment, mapping = aes(x = Title, y = abs(Amount))) +
+  geom_col(mapping = aes(fill = Title)) +
+  facet_wrap(~ Date)
