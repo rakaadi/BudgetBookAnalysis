ae30baeafbecbbbcd34baed12a824651f1e81a64
Raka <56627679+rakaadi@users.noreply.github.com>
2019-11-17 15:13:06 +0700

    Add files via upload
    Updated version. November 17th

--- Source code.R
+++ Source code.R
@@ -1,6 +1,8 @@
 # Library yang digunakan
 library(tidyverse)
 library(lubridate)
+library(scales)
+library(forcats)
 library(plotly)
 
 # Membaca file csv
@@ -32,17 +34,99 @@ investment <- select(investment, -Subcategory)
 # Mengambil data pemasukan 
 income <- filter(budget, Main_category == "Income")
 
+income$Day <- wday(income$Date, label = TRUE, abbr = FALSE)
+income$Month <- format(as_date(income$Date), '%Y-%m')
+
 # Plotting
   # Spending over time
 spending_data %>%
   ggplot(mapping = aes(x = Date, y = abs(Amount))) +
   geom_line() +
-  labs(y = "Amount") -> P
+  labs(y = "Amount") +
+  scale_y_continuous(labels = comma) -> P
 
     # Convert to plotly to see detail metadata 
 ggplotly(P)
 
-  # investment
-ggplot(data = investment, mapping = aes(x = Title, y = abs(Amount))) +
+  # Spending / month by category
+spending_data %>%
+  group_by(Month = month(Date), Year = year(Date), Main_category) %>%
+  summarise(Amount = sum(abs(Amount))) %>%
+  ggplot(mapping = aes(x = Month, y = Amount, fill = Main_category)) +
+  geom_bar(stat = 'identity', position = position_dodge()) +
+  labs(x = "", title = "Spending/Month", subtitle =  "by Category", fill = "Category") +
+  scale_x_continuous(
+    breaks = c(1, 2, 3, 4, 5, 6, 7, 8), 
+    label = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")) +
+  scale_y_continuous(labels = comma) -> P2
+  
+  # Mean Speding / category
+spending_data %>%
+  ggplot(mapping = aes(x = Main_category, y = abs(Amount))) +
+  geom_boxplot() +
+  scale_y_continuous(labels = comma) +
+  coord_flip() -> P3
+
+  # Balance adjustment category
+spending_data %>%
+  filter(Main_category %in% c("Balance Adjustment", "Compensation transactions")) %>%
+  ggplot(mapping = aes(x = Month, y = Amount, fill = Title)) +
+  geom_col(position = position_dodge2()) +
+  labs(title = "Balance Adjustment") +
+  scale_y_continuous(labels = comma) +
+  scale_fill_discrete(name = "") -> P4
+
+# investment
+ggplot(investment, mapping = aes(x = Title, y = abs(Amount))) +
   geom_col(mapping = aes(fill = Title)) +
-  facet_wrap(~ Date)
+  labs(x = "", y = "Amount", title = "Invesment/Month") +
+  scale_y_continuous(labels = comma) +
+  scale_fill_discrete(name = "") + 
+  theme(legend.position = 'none') +
+  facet_wrap(. ~ Month) -> P5
+
+# income
+income %>%  
+  group_by(Month = month(Date)) %>%
+  summarise(Amount = sum(Amount)) %>%
+  ggplot(mapping = aes(x = Month, y = Amount)) +
+  geom_col(fill = 'darkgreen', position = position_identity()) +
+  labs(x = "", title = 'Income/Month') +
+  scale_x_continuous(
+    breaks = c(1, 2, 3, 4, 5, 6, 7, 8),
+    label = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug")) +
+  scale_y_continuous(labels = comma) -> P6
+
+# Frequency plot
+  # Main_category frequency
+ggplot(budget, mapping = aes(x = fct_rev(fct_infreq(Main_category)))) +
+  geom_bar() +
+  labs(x = "Main Category") +
+  coord_flip() -> P7
+
+  # Subcategory frequency
+ggplot(budget, mapping = aes(x = fct_rev(fct_infreq(Subcategory)))) +
+  geom_bar() +
+  labs(x = "Subcategory") +
+  coord_flip() -> P8
+
+  # Main_category w/ Subcategory 
+budget %>%
+  filter(Subcategory != is.na(NA)) %>%
+  ggplot(mapping = aes(fct_rev(fct_infreq(Main_category)))) +
+  geom_bar(aes(fill = Subcategory), position = position_identity()) +
+  labs(x = "Main Category", 
+       title = "Main Category Frequency", 
+       subtitle = "with assigned Subcategory") +
+  coord_flip() -> P9
+
+  # Main_category wo/  Subcategory
+budget %>%
+  replace_na(list(Subcategory = "None")) %>%
+  filter(Subcategory == "None") %>%
+  ggplot(mapping = aes(fct_rev(fct_infreq(Main_category)))) +
+  geom_bar(position = position_dodge2()) +
+  labs(x = "Main Category",
+       title = "Main Category Frequency", 
+       subtitle = "with unassigned Subcategory") +
+  coord_flip() -> P10
